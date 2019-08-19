2356dee44505e6c73f34ee42a7be9f61e4ffa85c
Jack.W <jack.wanda@dragonsborn.net>
2019-08-19 17:41:02 +0800

    up

--- Convolutional Neural Networks/Week2/ResNets/resnet.py
+++ Convolutional Neural Networks/Week2/ResNets/resnet.py
@@ -1,20 +1,5 @@
 # coding: utf-8
 
-# # Residual Networks
-# 
-# Welcome to the second assignment of this week! You will learn how to build very deep convolutional networks, using Residual Networks (ResNets). In theory, very deep networks can represent very complex functions; but in practice, they are hard to train. Residual Networks, introduced by [He et al.](https://arxiv.org/pdf/1512.03385.pdf), allow you to train much deeper networks than were previously practically feasible.
-# 
-# **In this assignment, you will:**
-# - Implement the basic building blocks of ResNets. 
-# - Put together these building blocks to implement and train a state-of-the-art neural network for image classification. 
-# 
-# This assignment will be done in Keras. 
-# 
-# Before jumping into the problem, let's run the cell below to load the required packages.
-
-# In[8]:
-
-
 import numpy as np
 from keras import layers
 from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
